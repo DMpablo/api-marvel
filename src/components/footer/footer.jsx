4ffa76d94563ce3e379e92af7b6499d9e879b709
DMpablo <phdemarco@gmail.com>
2021-08-30 19:40:42 -0300

    footer add

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,14 +6,22 @@ export const Footer = ()=>{
 
     return (
     <div className={styles.containerFooter}>
-        <p>Este es un proyecto que solo tiene como objetivo desarrollar skills como developer.</p>
-        <Link to="https://developer.marvel.com/"> 
-            La API utilizada se encuentra en el sitio oficial de Marvel
-        </Link>
-
-        <Link to="https://developer.marvel.com/"> 
-            Dmpablo GitHub
-        </Link>
+        <div className={styles.declaracion}>
+            <p>Este es un proyecto que solo tiene como objetivo desarrollar skills como developer.</p>
+            <Link to="https://developer.marvel.com/"> 
+            <p>La API utilizada se encuentra en el sitio oficial de Marvel</p>
+            </Link>
+        </div>
+        <div className={styles.redes}>
+            <Link to="https://github.com/DMpablo"> 
+                <p>GitHub: DMpablo</p> 
+            </Link>
+            <Link to="https://www.linkedin.com/in/dmpablo/"> 
+                 <p>Linkedin: Pablo De Marco</p>
+            </Link>
+        </div>
+       
+      
         
     </div>)
 
